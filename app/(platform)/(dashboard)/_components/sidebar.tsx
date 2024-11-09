6aef5d956868831f64a6610cbfe7f2819d6fff52
lilseniorj <jesusvarguer18@gmail.com>
2024-11-09 16:15:34 -0500

    Workspace Settings

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -54,6 +54,11 @@ function Sidebar({ storageKey = "t-sidebar-state" }: SidebarProps) {
           <Skeleton className="h-10 w-[50%]" />
           <Skeleton className="h-10 w-10" />
         </div>
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
       </>
     );
   }
